f210188808121ff3fb1e7dfeca47d37af4b0c4d7
leroy12 <leroygonsalves12@gmail.com>
2018-03-15 14:25:49 +0530

    new project

--- MVCAppln_1/ViewModel/EmployeeListViewModel.cs
+++ MVCAppln_1/ViewModel/EmployeeListViewModel.cs
@@ -5,10 +5,11 @@ using System.Web;
 
 namespace MVCAppln_1.ViewModel
 {
-    public class EmployeeListViewModel
+    public class EmployeeListViewModel : BaseViewModel
     {
         public List<EmployeeViewModel> Employee {get; set;}
-        public string UserName { get; set; }
-        public FooterViewModel FooterData { get; set; }
+        //public string UserName { get; set; }
+       // public FooterViewModel FooterData { get; set; }
+
     }
 }
